e5edfa32994fa7f5417704e5c93636c4df29f86e
Anarasty <murloc.ninja16@gmail.com>
2022-05-21 17:36:00 +0300

    almost done movie main page

--- app.js
+++ app.js
@@ -55,68 +55,21 @@ const watchMovieData = (movies) => {
     const movieID = movie.imdbID;
     const inFavList = IDsList.includes(movieID);
     return `
-      <div class="movie-result">
-        <a class="movie-poster" href="https://www.imdb.com/title/${movieID}/"> 
-        <img
-          src="${movieIMG === "N/A" ? "images/default-movie-poster.jpg" : movieIMG}"
-          alt="movie poster"
-          width="300"
-          height="447"
-        />
-        </a> 
-        <div class="title-rating">
-          <h3 class="movie-title"><a href="https://www.imdb.com/title/${movieID}/">${movieTitle}</a></h3>
-          <span class="movie-rating"
-            ><img
-              class="rating-icon"
-              src="images/star-icon.svg"
-              width="15"
-              height="15"
-              alt="rating icon"
-            /> ${movieRating}</span
-          >
-        </div>
-        <div class="movie-details">
-          <span class="movie-duration">${movieDuration}</span>
-          <span class="movie-genre">${movieGenre}</span>
-          <button class="${
-            !inFavList ? "add-favlist" : "remove-favlist"
-          }" data-id="${movieID}">
-            <svg
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              class="plus-icon"
-            >
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM9 5C9 4.44772 8.55228 4 8 4C7.44772 4 7 4.44772 7 5V7H5C4.44772 7 4 7.44771 4 8C4 8.55228 4.44772 9 5 9H7V11C7 11.5523 7.44772 12 8 12C8.55228 12 9 11.5523 9 11V9H11C11.5523 9 12 8.55228 12 8C12 7.44772 11.5523 7 11 7H9V5Z"
-              />
-            </svg>
-            <svg
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                class="remove-icon"
-              >
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM5 7C4.44772 7 4 7.44772 4 8C4 8.55228 4.44772 9 5 9H11C11.5523 9 12 8.55229 12 8C12 7.44772 11.5523 7 11 7H5Z"
-                />
-              </svg>
-            Watchlist
-          </button>
-        </div>
-        <p class="movie-summary">
-          ${plot}
-        </p>
-      </div>
+    <div class="movie-result">
+    <img src="${movieIMG === "N/A" ? "images/default-movie-poster.jpg" : movieIMG}" alt="movieIMG">
+  
+    <div class="card-info">
+      <h2 class="card-title">${movieTitle}</h2>
+    </div>
+    <div class="card-info-more">
+      <h4 class="card-duration">Duration: ${movieDuration}</h4>
+      <h4 class="card-genre">Genres: ${movieGenre}</h4>
+    </div>
+    <div class="card-plot-box">
+      <p class="card-plot">${plot}</p>
+    </div>
+    <div class="card-btn-box"><button class="${!inFavList ? "add-favlist" : "remove-favlist"}" data-id="${movieID}">Favorites</button></div>
+  </div>
     `;
   });
 
@@ -128,7 +81,7 @@ const watchMovieData = (movies) => {
   if (movieMissing) {
     movieMissing.style.display = "none";
   }
-  movieContent.style.display = "block";
+  movieContent.style.display = "grid";
 };
 
 const resultDisplayError = () => {
